190762fa1cb8faf46259025a8c78b029a3ab779a
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-02-06 16:19:17 -0700

    added in graphql tagline and headlines

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-  ],
-}
+	plugins: [
+		"gatsby-plugin-resolve-src",
+		"gatsby-plugin-sass",
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/assets/images`,
+			},
+		},
+		{
+			resolve: `gatsby-source-graphcms`,
+			options: {
+				endpoint: `https://api-us-west-2.graphcms.com/v2/ckkubhqtg25v401z68kq86xzc/master`,
+			},
+		},
+	],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Layout from "components/Layout";
+import Container from "components/Container";
 
-import img_gatsby from 'assets/images/gatsby-astronaut.png';
+import img_gatsby from "assets/images/gatsby-astronaut.png";
 
 const IndexPage = () => {
-  return (
-    <Layout pageName="home">
-      <Helmet>
-        <title>Home Page</title>
-      </Helmet>
-      <Container>
-        <p className="gatsby-astronaut">
-          <img src={img_gatsby} alt="Build with Gatsby!" />
-        </p>
-        <h1>Gatsby Sass Starter</h1>
-        <p>
-          Welcome to your new Gatsby site.
-        </p>
-        <p>
-          Now go build something great.
-        </p>
-        <h2>Still Getting Started?</h2>
-        <p>
-          Run the following in your terminal!
-        </p>
-        <pre>
-          <code>
-            gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-sass
-          </code>
-        </pre>
-      </Container>
-    </Layout>
-  );
+	const { graphCmsPage = {} } = useStaticQuery(graphql`
+		query PageQuery {
+			graphCmsPage(id: { eq: "Page:ckkubui888h720b74z6lj5n2a" }) {
+				tagline
+				headline
+				id
+			}
+		}
+	`);
+
+	const { headline, tagline } = graphCmsPage;
+
+	return (
+		<Layout pageName="home">
+			<Helmet>
+				<title>Home Page</title>
+			</Helmet>
+			<Container>
+				<p className="gatsby-astronaut">
+					<img src={img_gatsby} alt="Build with Gatsby!" />
+				</p>
+				<h1>{headline}</h1>
+				<p>{tagline}</p>
+				<h2>Still Getting Started?</h2>
+				<p>Run the following in your terminal!</p>
+				<pre>
+					<code>
+						gatsby new [directory]
+						https://github.com/colbyfayock/gatsby-starter-sass
+					</code>
+				</pre>
+			</Container>
+		</Layout>
+	);
 };
 
 export default IndexPage;
